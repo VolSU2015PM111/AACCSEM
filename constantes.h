4bbae45cbdae27326570bba4d4982bd8bb1dfe62
PetuchovAlexey <petuchov.alexey@yandex.ru>
2015-06-18 03:24:19 +0400

    Revert "version 0"
    This reverts commit 106b0a51122db1b64b8ae71c3fe77e94ea674b34.

--- constantes.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-namespace EyeMomentClassification
-{
-	enum eyeMomentsClasses { fixation = 0, saccade = 1, NClasses = 2 };
-	enum eyeMomentsHidenStates{ slow = 0, fast = 1, NStates = 2 };
-}
