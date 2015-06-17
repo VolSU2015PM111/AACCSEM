458225ef83201e96a6e8e6a8e28b42f07920a320
PetuchovAlexey <petuchov.alexey@yandex.ru>
2015-06-18 03:23:04 +0400

    Revert "version 0.000"
    This reverts commit 6de018258ad915dc4fd9adf60e484d4ba8b59755.

--- EyeClassificationMacroses.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#ifdef ALGORITHMCLASSDLL_EXPORTS
-#define ALGORITHMCLASSDLL_API __declspec(dllexport) 
-#else
-#define ALGORITHMCLASSDLL_API __declspec(dllimport) 
-#endif
-
-
-#define SQRT(a) ((a)*(a))
--- functions.h
+++
@@ -1,8 +0,0 @@
-#include <vector>
-#include "EyeClassificationMacroses.h"
-
-namespace EyeMomentClassification
-{
-	double mathematicalExpectation(const std::vector<double> &Vector, const double& coof);
-	double dispersion(const std::vector<double> &Vector, const double &M);
-}
